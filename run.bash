8a3da745a6ac846d7cd7b619df27c81d17d841e3
Ben Hall <ben1734.hall@gmail.com>
2017-05-18 07:48:41 -0500

    Create run.bash

---
+++ run.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#import functions
+source ~/Desktop/ubuntu-script-cp2/modules/update
+
+clear
+
+#ask for functions to run
+echo answer with y/n
+echo -n "update?"
+read updateq
+clear
+
+#call to functions
+updatef
