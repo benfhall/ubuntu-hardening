da750b4323a1566fb745cde7d1ef2d9f86a4182a
Ben Hall <ben1734.hall@gmail.com>
2018-01-23 16:28:08 -0600

    added sudo for command

--- scripts/network.sh
+++ scripts/network.sh
@@ -2,7 +2,7 @@ function f_network {
   echo "Configuring network settings"
 
   sudo echo "nospoof on" >> /etc/host.conf
-  sed -i 's/net.ipv4.ip_forward=1/net.ipv4.ip_forward=0/' /etc/systcl.conf
+  sudo sed -i 's/net.ipv4.ip_forward=1/net.ipv4.ip_forward=0/' /etc/systcl.conf
   sudo sysctl -p
 
 }
