330cbf46cedf74abd6e1199f4298af4a5d589c45
Ben Hall <ben1734.hall@gmail.com>
2018-01-23 15:56:46 -0600

    fix
    fixed network file

--- scripts/network.sh
+++ scripts/network.sh
@@ -1,13 +1,8 @@
 function f_network {
   echo "Configuring network settings"
 
-  sudo echo 0 > /proc/sys/net/ipv4/ip_forward
-  sudo echo "order bind,hosts" >> /etc/host.conf
   sudo echo "nospoof on" >> /etc/host.conf
-  sudo echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
-  sudo echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
-  sudo echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
-  sudo sysctl -n net.ipv4.tcp_syncookies
+  sed -i 's/net.ipv4.ip_forward=1/net.ipv4.ip_forward=0/' /etc/systcl.conf
   sudo sysctl -p
 
 }
