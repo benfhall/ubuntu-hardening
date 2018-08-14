195770aa341eff82b6230c2e40f882d87d83e8f7
Ben Hall <swidexx@gmail.com>
2018-08-14 09:52:05 -0500

    Update network.sh

--- scripts/network.sh
+++ scripts/network.sh
@@ -3,6 +3,17 @@ function f_network {
   
   sudo sed -i 's/net.ipv4.ip_forward = 1/net.ipv4.ip_forward = 0/' /etc/sysctl.conf
   sudo sed -i 's/net.ipv4.tcp_syncookies = 1/net.ipv4.tcp_syncookies = 0/' /etc/sysctl.conf
+  
+  sudo sed -i 's/net.ipv4.conf.all.rp_filter = 0/net.ipv4.conf.all.rp_filter = 1/' /etc/sysctl.conf
+  sudo sed -i 's/net.ipv4.conf.default.rp_filter = 0/net.ipv4.conf.default.rp_filter = 1/' /etc/sysctl.conf
+  
+  sudo sed -i 's/net.ipv4.icmp_echo_ignore_broadcasts = 0/net.ipv4.icmp_echo_ignore_broadcasts = 1/' /etc/sysctl.conf
+  
+  sudo sed -i 's/net.ipv4.conf.all.accept_source_route = 1/net.ipv4.conf.all.accept_source_route = 0/' /etc/sysctl.conf
+  sudo sed -i 's/net.ipv6.conf.all.accept_source_route = 1/net.ipv6.conf.all.accept_source_route = 0 /' /etc/sysctl.conf
+  sudo sed -i 's/net.ipv4.conf.default.accept_source_route = 1/net.ipv4.conf.default.accept_source_route = 0/' /etc/sysctl.conf
+  sudo sed -i 's/net.ipv6.conf.default.accept_source_route = 1/net.ipv6.conf.default.accept_source_route = 0/' /etc/sysctl.conf
+  
   sudo sed -i 's/net.ipv6.conf.all.accept_ra = 1/net.ipv6.conf.all.accept_ra = 0/' /etc/sysctl.conf
   sudo sed -i 's/net.ipv6.conf.default.accept_ra = 1/net.ipv6.conf.default.accept_ra = 0/' /etc/sysctl.conf
   sudo sed -i 's/net.ipv4.conf.default.accept_redirects = 1/net.ipv4.conf.default.accept_redirects = 0/' /etc/sysctl.conf
