70a8d2a3d6e01d17bb317a3ff0755c435b03b129
Ben Hall <swidexx@gmail.com>
2019-11-21 14:30:43 -0600

    Update apt.sh

--- scripts/apt.sh
+++ scripts/apt.sh
@@ -1,7 +1,24 @@
 function f_apt {
 
-  mv /etc/apt/sources.list  /etc/apt/sources.list.old
-  mv defaults/sources.list /etc/apt/sources.list
+mv /etc/apt/sources.list  /etc/apt/sources.list.old
+
+version=$(lsb_release -a) 
+
+if [[ $version =~ .*14.04* ]]
+then
+    mv defaults/sources14.list /etc/apt/sources.list
+else
+    if [[ $version =~ .*16.04* ]]
+    then
+        mv defaults/sources16.list /etc/apt/sources.list
+    else
+        echo "UNKNOWN VERSION! ENDING SCRIPT"
+        exit
+    fi
+fi
+
+  
+  
   
   apt-get update -y
 
