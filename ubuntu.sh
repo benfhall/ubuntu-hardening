619ffd3c9044480ff00477b56cdf2e5d1230a920
Ben Hall <swidexx@gmail.com>
2019-11-21 14:27:45 -0600

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -7,10 +7,6 @@
 # PLAT:  linux-64
 # PLAT-Version: linux-14.04
 
-exec 3>&1 4>&2
-trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>log.out 2>&1
-
 chmod 755 scripts -R #ensure source can access modules
 
 source scripts/apache.sh
