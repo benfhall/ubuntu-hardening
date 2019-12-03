c15f90b8e8678c9343f762fecfefa00f9c85e9c6
Ben Hall <swidexx@gmail.com>
2019-12-03 12:12:00 -0600

    Update samba.sh

--- scripts/samba.sh
+++ scripts/samba.sh
@@ -2,10 +2,13 @@ function f_samba {
 
     apt-get install samba  -y
 
-    echo "hosts allow = 127.0.0.1" >> /etc/samba/smb.conf
+    echo "hosts allow = 127.0.0.1 192.168.2.0/24 192.168.3.0/24" >> /etc/samba/smb.conf
     echo "hosts deny = 0.0.0.0/0" >> /etc/samba/smb.conf
     echo "interfaces = eth* lo" >> /etc/samba/smb.conf
     echo "bind interfaces only = yes" >> /etc/samba/smb.conf
+    echo "guest ok = no" >> /etc/samba/smb.conf
+    echo 'restrict anonymous = 2' >> /etc/samba/smb.conf
+    echo 'encrypt passwords = yes' >> /etc/samba/smb.conf
 
     ufw allow samba
 
