b87e22572f326d9a5e819cc7ea9ff6c74ecc2503
Ben Hall <swidexx@gmail.com>
2019-12-03 13:58:46 -0600

    Update apt.sh

--- scripts/apt.sh
+++ scripts/apt.sh
@@ -4,6 +4,10 @@ mv /etc/apt/sources.list  /etc/apt/sources.list.old
 
 version=$(lsb_release -a) 
 
+apt-get install unattended-upgrades -y
+echo 'APT::Periodic::Update-Package-Lists "1";' >> /etc/apt/apt.conf.d/20auto-upgrades
+echo 'APT::Periodic::Unattended-Upgrade "1";' >> /etc/apt/apt.conf.d/20auto-upgrades
+
 if [[ $version =~ .*14.04* ]]
 then
     mv defaults/sources14.list /etc/apt/sources.list
