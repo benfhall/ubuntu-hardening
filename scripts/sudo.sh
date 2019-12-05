9d34e81b7a854d1e66f2677c369f894389a4d825
Ben Hall <swidexx@gmail.com>
2019-12-05 13:58:07 -0600

    Update sudo.sh

--- scripts/sudo.sh
+++ scripts/sudo.sh
@@ -1,7 +1,7 @@
 function f_sudo {
 
 	apt-get install sudo -y
-	sed -i "s/#.*auth.*required.*pam_wheel\.so/auth required pam_wheel\.so group=wheel debug/" /etc/pam.d/su
+	echo 'auth required pam_wheel.so use_uid' >> /etc/pam.d/su
 	echo "%wheel  ALL=(ALL:ALL) ALL" >> /etc/sudoers
 
 }
