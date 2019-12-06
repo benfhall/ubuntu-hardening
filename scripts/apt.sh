e8fe8fb92eae96d8ebc54a5415ae5a9603a6cacd
Ben Hall <swidexx@gmail.com>
2019-12-05 19:38:33 -0600

    fixing software & updates options

--- scripts/apt.sh
+++ scripts/apt.sh
@@ -5,8 +5,8 @@ mv /etc/apt/sources.list  /etc/apt/sources.list.old
 version=$(lsb_release -a) 
 
 apt-get install unattended-upgrades -y
-echo 'APT::Periodic::Update-Package-Lists "1";' >> /etc/apt/apt.conf.d/20auto-upgrades
-echo 'APT::Periodic::Unattended-Upgrade "1";' >> /etc/apt/apt.conf.d/20auto-upgrades
+grep -q '^APT::Periodic::Update-Package-Lists' /etc/apt/apt.conf.d/20auto-upgrades && sed -i 's/^APT::Periodic::Update-Package-Lists.*/APT::Periodic::Update-Package-Lists "1";/' /etc/apt/apt.conf.d/20auto-upgrades || echo 'APT::Periodic::Update-Package-Lists "1";' >> /etc/apt/apt.conf.d/20auto-upgrades
+grep -q '^APT::Periodic::Unattended-Upgrade' /etc/apt/apt.conf.d/20auto-upgrades && sed -i 's/^APT::Periodic::Unattended-Upgrade.*/APT::Periodic::Unattended-Upgrade "1";/' /etc/apt/apt.conf.d/20auto-upgrades || echo 'APT::Periodic::Unattended-Upgrade "1";' >> /etc/apt/apt.conf.d/20auto-upgrades
 
 if [[ $version =~ .*14.04* ]]
 then
